0106abcf626dc120357522bf741583e6ae621bfe
Thomas Ilene <44019338+thoilene@users.noreply.github.com>
2020-07-15 00:11:02 +0200

    Python script to run the web app
    Python script to run the web app

--- app/run.py
+++ app/run.py
@@ -7,8 +7,10 @@ from nltk.tokenize import word_tokenize
 
 from flask import Flask
 from flask import render_template, request, jsonify
-from plotly.graph_objs import Bar
-from sklearn.externals import joblib
+from plotly.graph_objs import Bar,Heatmap
+from plotly.graph_objs.layout import Annotation 
+#from sklearn.externals import joblib
+import joblib
 from sqlalchemy import create_engine
 
 
@@ -24,6 +26,22 @@ def tokenize(text):
         clean_tokens.append(clean_tok)
 
     return clean_tokens
+
+def disaster_counts(df_):
+    Y_ = df_.drop(['id','message','original','genre'],axis=1)
+    disaster_0 = []
+    disaster_1 = []
+    disaster_2 = []
+    disaster_1_2 = []
+    for col in Y_.columns:
+        disaster_0.append(Y_[Y_[col]==0].shape[0])
+        disaster_1.append(Y_[Y_[col]==1].shape[0])
+        disaster_2.append(Y_[Y_[col]==2].shape[0])
+        disaster_1_2.append(Y_[Y_[col]==1].shape[0]+Y_[Y_[col]==2].shape[0])
+    
+    return pd.DataFrame(index=Y_.columns, data={'disasters_type_0':disaster_0,'disasters_type_1':disaster_1,'disasters_type_2':disaster_2,'disasters_type_1_2':disaster_1_2})
+
+
 
 # load data
 engine = create_engine('sqlite:///../data/DisasterResponse.db')
@@ -42,6 +60,27 @@ def index():
     # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
+    
+    d_counts = disaster_counts(df)
+    
+    categories = list(d_counts.index)
+    disasters_type_1 = list(d_counts['disasters_type_1'].values)
+    disasters_type_1_2 = list(d_counts['disasters_type_1_2'].values)
+    
+    
+    z = df[['related','request','aid_related','weather_related','direct_report']].corr()
+    z = z.values
+    x=['related','request','aid_related','weather_related','direct_report']
+    y=['related','request','aid_related','weather_related','direct_report']
+    z_vals = z
+    
+    annotations = []
+    for n, row in enumerate(z):
+        #print(n, row)
+        for m, val in enumerate(row):
+            #print(n,m,z[n][m],x[m],y[n])
+            annotations.append(Annotation(text="{:.2f}".format(z[n][m]), x=x[m], y=y[n],
+                                             xref='x1', yref='y1', showarrow=False))
     
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
@@ -62,6 +101,48 @@ def index():
                 'xaxis': {
                     'title': "Genre"
                 }
+            }
+        },
+        # second element
+        {
+            'data': [
+                Bar(
+                    x=categories,
+                    y=disasters_type_1,
+                    name="type 1 only"
+                ),
+                Bar(
+                    x=categories,
+                    y=disasters_type_1_2,
+                    name="type 1/2"
+                )
+            ],
+
+            'layout': {
+                'title': 'Disaster Count (types 1 and 2)',
+                'yaxis': {
+                    'title': "Disaster Count"
+                },
+                'xaxis': {
+                    'title': "Category"
+                }
+            }
+        },
+        # third element
+        {
+            'data': [
+                Heatmap(
+                   z = z_vals,
+                   x=['related','request','aid_related','weather_related','direct_report'],
+                   y=['related','request','aid_related','weather_related','direct_report'],
+                   colorscale=[[0.0, '#F5FFFA'],[0.2, '#ADD8E6'], [0.4, '#87CEEB'],[0.6, '#87CEFA'], [0.8, '#40E0D0'], [1.0, '#00CED1']],
+                   hoverongaps = False)
+            ],
+
+            'layout': {
+                'title': 'Correlations between the most important categories',
+                'annotations':annotations,
+                
             }
         }
     ]
@@ -93,7 +174,7 @@ def go():
 
 
 def main():
-    app.run(host='0.0.0.0', port=3001, debug=True)
+    app.run(host='127.0.0.1', port=3001, debug=True)
 
 
 if __name__ == '__main__':
